0bf1f94221c54d58cf7d8440103ab9e6a6e1dc36
Jerry <jerryhxie@gmail.com>
2025-02-03 12:28:53 -0700

    Working Commit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -63,7 +64,10 @@ public class RobotContainer {
     
     new JoystickButton(m_driverController.getHID(), DriveConstants.k_A)
       .onTrue(
-        new InstantCommand(() -> m_elevatorSubsystem.setPosition(ElevatorConstants.k_coralL1Height), m_elevatorSubsystem)
+        new SequentialCommandGroup(
+          new InstantCommand(() -> m_elevatorSubsystem.setPosition(ElevatorConstants.k_zeroHeight), m_elevatorSubsystem),
+          new InstantCommand(() -> m_elevatorSubsystem.setNeutral(), m_elevatorSubsystem)
+        )
       );
 
     new JoystickButton(m_driverController.getHID(), DriveConstants.k_B)
