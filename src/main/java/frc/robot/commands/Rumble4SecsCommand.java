ea7963c02f76b4ab88ec0c9136f7dd7d3ce79a60
Jet Webster <jetwebster@students.waterfordschool.org>
2025-02-14 16:42:35 -0700

    Finished Command 4 Run4SecsCommand.

--- src/main/java/frc/robot/commands/Rumble4SecsCommand.java
+++ src/main/java/frc/robot/commands/Rumble4SecsCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
@@ -11,5 +12,24 @@ public class Rumble4SecsCommand extends Command{
     public Rumble4SecsCommand(double s, CommandXboxController c){
         m_run4Secs = s;
         m_controller = c;
+    }
+
+    @Override
+    public void initialize(){
+        timer.start();
+        timer.reset();
+    }
+
+    @Override
+    public void execute(){
+        m_controller.getHID().setRumble(RumbleType.kBothRumble, 1);
+    }
+
+    @Override
+    public void end(boolean useless){
+        m_controller.getHID().setRumble(RumbleType.kBothRumble, 0);
+    }
+    public boolean isFinished(){
+        return timer.hasElapsed(m_run4Secs);
     }
 }
